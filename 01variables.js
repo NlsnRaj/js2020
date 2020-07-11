8895d403d8323f3984570a3d315391b8df562d01
nlsn raj <nlsn.raj95@gmail.com>
2020-07-11 17:29:47 +0530

    first commit

---
+++ 01variables.js
@@ -0,0 +1,23 @@
+var fullName ="Nelso Raj";
+
+// How to Name Variables?
+// FullName?
+// full-name?
+// full_name?
+
+// Good Practice is camalCase fullName
+
+var emaidId = "nlsn.raj95@gmail.com";
+console.log(emaidId);
+
+var isLoggedIn = "false" //dont use False because js has different meaning for it 
+
+var number ="123456789"
+console.log(number);
+
+console.log("number");// run & see The Difference 
+
+
+
+
+
---
+++ signup.js
@@ -0,0 +1,22 @@
+const uid ="abc123"; // its means constant u cant simply change the uid value
+
+
+var firstName = "Nelson";
+var lastName ="Raj";
+var email ="jstutut@gmail.com";
+var password ="12345";
+var confirmPassword ="12345";
+var courseCount =0;
+var isLoggedInGoogle ="false";
+var isLoggedInFb ="false";
+
+console.log(uid);
+console.log(firstName);
+//try the below method is Interpolation
+console.log(
+    `Unique Id: ${uid}
+     FirstName: ${firstName}
+     LastName:${lastName}
+     Email:${email}`
+);
+
